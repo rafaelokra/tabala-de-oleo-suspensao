b36a768a551bfb829d9e0093eac2441a32dec7e5
Rafaelokra <okra1002@gmail.com>
2025-03-10 21:46:06 -0300

    atualizando banco de dados

--- js/database/motorcycles.js
+++ js/database/motorcycles.js
@@ -1,9 +1,10 @@
 // Banco de dados de motos e volumes de óleo de suspensão
 const motorcycleDatabase = [
   {
-    model: "honda cb 500f",
-    leftVolume: "355 ml",
-    rightVolume: "355 ml",
+    model: "honda cb 300",
+    leftVolume: "323 ml",
+    rightVolume: "323 ml",
+    fluidLevel: "128 ml",
     recommendation: "Utilize óleo de suspensão SAE 10W para melhor desempenho.",
   },
   {
@@ -13,42 +14,42 @@ const motorcycleDatabase = [
     recommendation: "Recomenda-se óleo de suspensão SAE 10W para este modelo.",
   },
   {
-    model: "yamaha mt-07",
-    leftVolume: "540 ml",
-    rightVolume: "540 ml",
-    recommendation:
-      "Para melhor desempenho, utilize óleo de suspensão SAE 15W.",
+    model: "honda falcon 400",
+    leftVolume: "529  ± 2,5 ml",
+    rightVolume: "529  ± 2,5 ml",
+    recommendation: "Utilize óleo de suspensão SAE 10W para melhor desempenho.",
   },
   {
-    model: "kawasaki ninja 300",
-    leftVolume: "410 ml",
-    rightVolume: "410 ml",
+    model: " hondaxre 190",
+    leftVolume: "176  ± 2,5 ml",
+    rightVolume: "176  ± 2,5 ml",
     recommendation: "Para suspensões mais macias, pode-se usar óleo SAE 10W.",
   },
   {
-    model: "suzuki gsx-s750",
-    leftVolume: "492 ml",
-    rightVolume: "492 ml",
+    model: "honda crf 230",
+    leftVolume: "380 ± 2,5 ml ",
+    rightVolume: "380 ± 2,5 ml",
     recommendation:
-      "Óleo de suspensão SAE 15W é o mais indicado para este modelo.",
+      "Óleo de suspensão SAE 10W é o mais indicado para este modelo.",
   },
   {
     model: "honda xre 300",
-    leftVolume: "550 ml",
-    rightVolume: "550 ml",
+    leftVolume: "547 ± 2,5 ml",
+    rightVolume: "547 ± 2,5 ml",
     recommendation:
-      "Para uso off-road mais intenso, considere óleo de suspensão SAE 10W.",
+      "Óleo de suspensão SAE 10W é o mais indicado para este modelo.",
   },
   {
-    model: "bmw g 310 gs",
-    leftVolume: "453 ml",
-    rightVolume: "453 ml",
-    recommendation: "Recomenda-se verificar o nível a cada 10.000 km.",
+    model: "honda pcx 160",
+    leftVolume: "116 ± 2,5 ml",
+    rightVolume: "116 ± 2,5 ml",
+    recommendation:
+      "Óleo de suspensão SAE 10W é o mais indicado para este modelo.",
   },
   {
-    model: "yamaha fazer 250",
-    leftVolume: "320 ml",
-    rightVolume: "320 ml",
+    model: "honda cb twister 250",
+    leftVolume: "306 ± 2,5 ml",
+    rightVolume: "306 ± 2,5 ml",
     recommendation:
       "Para melhor desempenho em estradas irregulares, use óleo SAE 10W.",
   },
@@ -59,9 +60,21 @@ const motorcycleDatabase = [
     recommendation: "Use óleo de suspensão SAE 10W neste modelo.",
   },
   {
-    model: "yamaha factor 150",
-    leftVolume: "170 ml",
-    rightVolume: "170 ml",
+    model: "honda crf 250",
+    leftVolume: "637 ± 2,5 ml",
+    rightVolume: "637 ± 2,5 ml",
+    recommendation: "Recomenda-se óleo de suspensão SAE 10W.",
+  },
+  {
+    model: "honda pop 110",
+    leftVolume: "58 ± 1 ml",
+    rightVolume: "58 ± 1 ml",
+    recommendation: "Recomenda-se óleo de suspensão SAE 10W.",
+  },
+  {
+    model: "honda cg 160 2016",
+    leftVolume: "139 ± 2,5 ml",
+    rightVolume: "139 ± 2,5 ml",
     recommendation: "Recomenda-se óleo de suspensão SAE 10W.",
   },
 ];
